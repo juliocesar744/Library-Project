ecdfdedadcd633bd6f8e1bdecbfd67950c7dfd00
Julio <jc-rl2@hotmail.com>
2024-06-23 12:45:36 +1000

    added controller and user service

---
+++ src/controllers/user/CreateUserController.ts
@@ -0,0 +1,18 @@
+import { Request, Response} from "express"
+import { UserRequest } from "../../models/interfaces/user/UserRequest";
+import { CreateUserService } from "../../services/user/CreateUserService";
+
+class CreateUserController {
+    async handle(request: Request, response: Response) {
+        const { username, password }: UserRequest = request.body;
+        const createUserService = new CreateUserService();
+        const user = await createUserService.execute({
+            username: username,
+            password: password
+        });
+
+        return response.json(user);
+    }
+}
+
+export {CreateUserController}
